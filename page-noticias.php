c0221e25ea4c81ca2abf4e9fd9c1a1f49366be68
Jonathan Meirelles <jonathanms30@gmail.com>
2019-08-04 22:00:41 -0300

    Wordpress
    Terminado tudo

--- page-noticias.php
+++ page-noticias.php
@@ -4,76 +4,75 @@
 
 <?php get_header(); ?>
 
-    <section class="our-blog">
-        <div class="container">
-            <div class="recent-posts">
-                <div class="title">
-                    <h2>Notícias recentes</h2>
-                </div>
-                <div class="description">
-                    <div class="posts">
-                        
-                        <?php
-							$artigo = new WP_Query(
-								array(
-									'posts_per_page'   => 3,
-									'post_type'        => 'artigo',
-									'post_status'      => 'publish',
-									'suppress_filters' => true,
-									'orderby'          => 'post_date',
-                                    'order'            => 'DESC',
-                                    'paged'            => max(1 , get_query_var('paged'))
-								)
-							);
-						?>
-						<?php if($artigo -> have_posts()):
-							while($artigo -> have_posts()): $artigo -> the_post(); ?>
+<section class="our-blog">
+    <div class="container">
+        <div class="recent-posts">
+            <div class="title">
+                <h2>Notícias recentes</h2>
+            </div>
+            <div class="description">
+                <div class="posts">
 
-								<div class="posts-item wow fadeInUp">
-									<a href="<?php the_permalink(); ?>">
-										<div class="posts-item-wrapper">
-											<div class="posts-item-img">
-												<img src="<?php the_field('imagem_noticia'); ?>" alt="<?php the_field('titulo_do_noticia'); ?>">
-											</div>
-										</div>
-										<h4><?php the_field('titulo_do_noticia'); ?></h4>
-										<p class="post-date"><?php the_field('data_do_noticia'); ?></p>
-										<p><?php the_field('resumo_do_noticia'); ?></p>
-									</a>
-								</div>
+                    <?php
+                    $artigo = new WP_Query(
+                        array(
+                            'posts_per_page'   => 3,
+                            'post_type'        => 'artigo',
+                            'post_status'      => 'publish',
+                            'suppress_filters' => true,
+                            'orderby'          => 'post_date',
+                            'order'            => 'DESC',
+                            'paged'            => max(1, get_query_var('paged'))
+                        )
+                    );
+                    ?>
+                    <?php if ($artigo->have_posts()) :
+                        while ($artigo->have_posts()) : $artigo->the_post(); ?>
 
-							<?php endwhile; ?>
-
-                            <div class="paginate">
-                                <?php
-                                    //chamar a paginacao
-                                    $total_pages = $artigo->max_num_pages;
-
-                                    if ($total_pages > 1){
-                                        $current_page = max(1, get_query_var('paged'));
-                                    
-                                        echo paginate_links(array(
-                                            'base' 		=> get_pagenum_link(1) . '%_%',
-                                            'format' 	=> '/page/%#%',
-                                            'current' 	=> $current_page,
-                                            'total' 	=> $total_pages,
-                                            'prev_next' => true,
-                                            'prev_text' => __('<'),
-                                            'next_text' => __('>')
-                                        ));
-                                    } 
-                                ?>
+                            <div class="posts-item wow fadeInUp">
+                                <a href="<?php the_permalink(); ?>">
+                                    <div class="posts-item-wrapper">
+                                        <div class="posts-item-img">
+                                            <img src="<?php the_field('imagem_noticia'); ?>" alt="<?php the_field('titulo_do_noticia'); ?>">
+                                        </div>
+                                    </div>
+                                    <h4><?php the_field('titulo_do_noticia'); ?></h4>
+                                    <p class="post-date"><?php the_field('data_do_noticia'); ?></p>
+                                    <p><?php the_field('resumo_do_noticia'); ?></p>
+                                </a>
                             </div>
 
+                        <?php endwhile; ?>
 
-						<?php else: ?>
-							<p>Nenhuma notícia disponível</p>
-						<?php endif; ?>
-						<?php wp_reset_postdata(); ?>
 
-                    </div>
+                    <?php else : ?>
+                        <p>Nenhuma notícia disponível</p>
+                    <?php endif; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                </div>
+                <div class="paginate">
+                    <?php
+                    //chamar a paginacao
+                    $total_pages = $artigo->max_num_pages;
+
+                    if ($total_pages > 1) {
+                        $current_page = max(1, get_query_var('paged'));
+
+                        echo paginate_links(array(
+                            'base'         => get_pagenum_link(1) . '%_%',
+                            'format'     => '/page/%#%',
+                            'current'     => $current_page,
+                            'total'     => $total_pages,
+                            'prev_next' => true,
+                            'prev_text' => __('<'),
+                            'next_text' => __('>')
+                        ));
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 <?php get_footer(); ?>
